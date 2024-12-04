6e5d23328ec66ca23d8ef6d072f9f17a3d9a09f8
ifeolaitan <ifeoluwa1201@gmail.com>
2024-12-04 23:40:26 +0000

    module directories

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
