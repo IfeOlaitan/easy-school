297ace7a8b889e6d126a432f4c320f18097f8baa
ifeolaitan <ifeoluwa1201@gmail.com>
2024-12-07 20:26:38 +0000

    updated modules

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0735c191cf914754d"  # Amazon Linux 2023 in us-west-2
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 instance will be launched"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID for EC2 instance"
+  type        = string
+}
+
+variable "docker_image" {
+  description = "Docker image to run on the instance"
+  type        = string
+}
