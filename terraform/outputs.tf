bbdecfffd31e551364fa0f260dde4bae4850ea7c
ifeolaitan <ifeoluwa1201@gmail.com>
2025-01-02 21:50:18 +0000

    update ami

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,7 +21,7 @@ output "instance_public_ip" {
   value       = module.compute.public_ip
 }
 
-#output "instance_public_dns" {
+# output "instance_public_dns" {
 #  description = "The public DNS name of our EC2 instance"
 #  value       = module.compute.public_dns
 #}
